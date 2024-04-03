50dd1612e4106a795c0e3f2179282fbcfe163005
Henry <hbreukelman20@gmail.com>
2024-04-03 10:55:59 -0500

    changed shuffle

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,7 +3,20 @@
 import * as utils from './utils.js';
 import {Score} from './Score.js';
 
-let words = ['dinosaur', 'love', ];
+let words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 
+'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money', 
+'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow', 
+'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 
+'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component', 
+'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 
+'agency', 'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 
+'magician', 'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 
+'evolution', 'banana', 'perfumer', 'computer', 'management', 'discovery', 
+'ambition', 'music', 'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 
+'enemy', 'button', 'superman', 'library', 'unboxing', 'bookstore', 'language', 
+'homework', 'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 
+'mystery', 'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 
+'yellow', 'keyboard', 'window'];
 
 /*
   selectors
